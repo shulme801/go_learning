91ff4bec51493650f51182bfa196629c54e811eb
Stephen Hulme <shulme801@gmail.com>
2020-03-29 12:13:21 -0400

    29 March commit 1

--- lissajous/lissajous.go
+++ lissajous/lissajous.go
@@ -12,19 +12,22 @@ import (
 	"os"
 )
 
-//var palette = []color.Color{color.White, color.Black, color.RGBA{0x7C, 0xFC, 0x00, 0xff}}
-var palette = []color.Color{color.Black, color.RGBA{0x7C, 0xFC, 0x00, 0xff}}
+
+var palette = []color.Color{color.White, color.RGBA{0x00, 0x00, 0x00, 0xff}, color.RGBA{0x7C, 0xFC, 0x00, 0xff}}
 const (
 	whiteIndex = 0 // first color in palette
 	blackIndex = 1 // next color in palette
 	greenIndex = 2 // our green color
 )
 
+
+
 func main() {
-	lissajous(os.Stdout)
+
+	lissajous(os.Stdout, blackIndex, whiteIndex)
 }
 
-func lissajous(out io.Writer) {
+func lissajous(out io.Writer, bkg uint8, lineColor uint8) {
 	const (
 		cycles	= 5 		// number of complete x oscillator revolutions
 		res		= 0.001		// angular resolution
@@ -32,17 +35,20 @@ func lissajous(out io.Writer) {
 		nframes = 64		// number of animation frames
 		delay   = 8			// delay between frames in 10 ms units
 	)
+	
+	var myPalette = []color.Color{palette[bkg], palette[lineColor]} // create a palette of 2 colors, with the background color in the 0th spot
+
+
 	freq	:= rand.Float64() * 3.0 	// relative frequency of y oscillator
 	anim 	:= gif.GIF{LoopCount: nframes}
 	phase 	:= 0.0
 	for i:= 0; i < nframes; i++ {
 		rect	:= image.Rect(0, 0, 2*size+1, 2*size+1)
-		myPalette := palette[blackIndex:greenIndex]
 		img 	:= image.NewPaletted(rect, myPalette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), lineColor)
 		}
 		phase		+=	0.1
 		anim.Delay	=	append(anim.Delay, delay)
---
+++ server1/server1.go
@@ -0,0 +1,20 @@
+// Server 1 is a minimal "echo" server.
+package main
+
+import (
+	"fmt"
+    "log"
+    "net/http"
+)
+   
+
+
+func main() {
+    http.HandleFunc("/", handler) //each request call handler
+    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echoes the Path component of the requested URL
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
