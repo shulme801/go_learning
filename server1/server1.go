357e8bd3add13cfe9fe9c3c17de125f68bb235f1
Stephen Hulme <shulme801@gmail.com>
2020-04-26 15:35:51 -0400

    26 April Commit C

--- server1/server1.go
+++ server1/server1.go
@@ -10,7 +10,7 @@ import (
 
 
 func main() {
-    http.HandleFunc("/", handler) //each request call handler
+    http.HandleFunc("/", handler) //each request calls handler
     log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
