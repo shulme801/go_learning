0f1def63a6f271ad6214dfadfbf60e18bd8473f6
Stephen Hulme <shulme801@gmail.com>
2020-04-28 08:27:55 -0400

    28 April A

--- server1/server1.go
+++ server1/server1.go
@@ -3,18 +3,16 @@ package main
 
 import (
 	"fmt"
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
-   
-
 
 func main() {
-    http.HandleFunc("/", handler) //each request calls handler
-    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	http.HandleFunc("/", handler) //each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 // handler echoes the Path component of the requested URL
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
